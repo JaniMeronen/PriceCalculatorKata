9ca0ac99d59a52ebfadb7399f55103f2b81ed5ff
Jani Meronen <jani.meronen@hotmail.com>
2021-05-28 20:41:07 +0300

    TAX

---
+++ PriceCalculatorKata/Product.cs
@@ -0,0 +1,17 @@
+﻿using static System.Decimal;
+
+namespace PriceCalculatorKata
+{
+    record Product(string Name, decimal Price, int Upc)
+    {
+        public Product ApplyTax(int rate)
+        {
+            if (rate == 0) return this;
+            var factor = rate / 100M;
+            var taxAmount = Price * factor;
+            var taxedPrice = Price + taxAmount;
+            var roundedPrice = Round(taxedPrice, 2);
+            return this with { Price = roundedPrice };
+        }
+    }
+}
--- PriceCalculatorKata/Program.cs
+++ PriceCalculatorKata/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using PriceCalculatorKata;
+using static System.Console;
+
+var sample = new Product("The Little Prince", 20.25M, 12345);
+
+WriteLine($"Sample product: Book with name = “{sample.Name}”, UPC = {sample.Upc}, price = ${sample.Price}.");
+WriteLine();
+
+var rate1 = 20;
+var taxedProduct1 = sample.ApplyTax(rate1);
+WriteLine($"Product price reported as ${sample.Price} before tax and ${taxedProduct1.Price} after {rate1}% tax.");
+
+var rate2 = 21;
+var taxedProduct2 = sample.ApplyTax(rate2);
+WriteLine($"Product price reported as ${sample.Price} before tax and ${taxedProduct2.Price} after {rate2}% tax.");
