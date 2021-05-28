51c6bda5419e20aae60f0e8205727f5ddb5e8a19
Jani Meronen <jani.meronen@hotmail.com>
2021-05-28 23:29:44 +0300

    PRECISION

--- PriceCalculatorKata/Product.cs
+++ PriceCalculatorKata/Product.cs
@@ -7,17 +7,17 @@ namespace PriceCalculatorKata
     {
         public Receipt CreateReceipt(Discount afterTaxDiscount, Discount beforeTaxDiscount, Amount Cap, IEnumerable<Expense> expenses, int tax)
         {
-            var beforeTaxDiscountAmount = beforeTaxDiscount.Apply(this);
-            var discountedPrice = Price - beforeTaxDiscountAmount;
+            var beforeTaxDiscountAmount = beforeTaxDiscount.Apply(this).Round(4);
+            var discountedPrice = (Price - beforeTaxDiscountAmount).Round(4);
             var discountedProduct = this with { Price = discountedPrice };
-            var taxAmount = discountedPrice.Scale(tax);
-            var afterTaxDiscountAmount = afterTaxDiscount.Apply(discountedProduct);
-            var uncappedDiscountAmount = beforeTaxDiscountAmount + afterTaxDiscountAmount;
-            var cappedDiscountAmount = Cap.Calculate(Price);
+            var taxAmount = discountedPrice.Scale(tax).Round(4);
+            var afterTaxDiscountAmount = afterTaxDiscount.Apply(discountedProduct).Round(4);
+            var uncappedDiscountAmount = (beforeTaxDiscountAmount + afterTaxDiscountAmount).Round(4);
+            var cappedDiscountAmount = Cap.Calculate(Price).Round(4);
             var discountAmount = uncappedDiscountAmount.Min(cappedDiscountAmount);
             var costs = expenses.Select(expense => expense.CalculateCost(Price));
-            var costsAmount = costs.Aggregate(Price.Zero, (total, expense) => total + expense.Amount);
-            var total = Price + taxAmount - discountAmount + costsAmount;
+            var costsAmount = costs.Aggregate(Price.Zero, (total, expense) => total + expense.Amount.Round(4));
+            var total = (Price + taxAmount - discountAmount + costsAmount).Round(4);
             return new(Price, discountAmount, costs, taxAmount, total);
         }
     }
--- PriceCalculatorKata/Program.cs
+++ PriceCalculatorKata/Program.cs
@@ -3,20 +3,16 @@ using PriceCalculatorKata.Amounts;
 using PriceCalculatorKata.Discounts;
 using static System.Console;
 
-var sample = new Product("The Little Prince", new(17.76M, "GBP"), 12345);
+var sample = new Product("The Little Prince", new(20.25M, "USD"), 12345);
 var universalDiscount = new UniversalDiscount(15);
 var upcDiscount = new UpcDiscount(7, 12345);
-var afterTaxDiscount = new AdditiveDiscounts(universalDiscount, upcDiscount);
+var afterTaxDiscount = new MultiplicativeDiscounts(universalDiscount, upcDiscount);
 
 var cap = new RelativeAmount(100);
 
-var expenses = new Expense[]
-{
-    //new(new RelativeAmount(1), "Packaging"),
-    //new(new AbsoluteAmount(new(2.2M, "GBP")), "Transport")
-};
+var expenses = new Expense[] { new(new RelativeAmount(3), "Transport") };
 
-var receipt = sample.CreateReceipt(new NoDiscount(), new NoDiscount(), cap, expenses, 20);
+var receipt = sample.CreateReceipt(afterTaxDiscount, new NoDiscount(), cap, expenses, 21);
 WriteLine($"Cost = ${receipt.Cost.Round(2)}");
 WriteLine($"Tax = ${receipt.Tax.Round(2)}");
 if (receipt.Discounts.Any) WriteLine($"Discounts = ${receipt.Discounts.Round(2)}");
