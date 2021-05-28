0fc940ab981b4d81a796d7ae9cc6e98be0c571f2
Jani Meronen <jani.meronen@hotmail.com>
2021-05-28 21:22:51 +0300

    REPORT

--- PriceCalculatorKata/Program.cs
+++ PriceCalculatorKata/Program.cs
@@ -2,12 +2,6 @@
 using static System.Console;
 
 var sample = new Product("The Little Prince", 20.25M, 12345);
-
-WriteLine($"Sample product: Book with name = “{sample.Name}”, UPC = {sample.Upc}, price = ${sample.Price}.");
-WriteLine();
-
-var receipt = sample.CreateReceipt(15, 20);
-
-WriteLine($"Tax = {receipt.Tax} %, discount = {receipt.Discount} %");
-WriteLine($"Tax amount = ${receipt.TaxAmount}, Discount amount = ${receipt.DiscountAmount}");
-WriteLine($"Price before = ${receipt.PriceBefore}, price after = ${receipt.PriceAfter}");
+var receipt = sample.CreateReceipt(0, 20);
+WriteLine($"${receipt.PriceAfter}");
+if (receipt.DiscountAmount > 0M) WriteLine($"${receipt.DiscountAmount}");
