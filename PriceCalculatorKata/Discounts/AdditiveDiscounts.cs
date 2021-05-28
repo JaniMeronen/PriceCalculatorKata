69570f02b4f549c8020da04abaeff81892478dc1
Jani Meronen <jani.meronen@hotmail.com>
2021-05-28 22:41:26 +0300

    COMBINING

--- PriceCalculatorKata/Discounts/AdditiveDiscounts.cs
+++ PriceCalculatorKata/Discounts/AdditiveDiscounts.cs
@@ -1,6 +1,6 @@
 ﻿namespace PriceCalculatorKata.Discounts
 {
-    record ComplexDiscount(Discount First, Discount Second) : Discount
+    record AdditiveDiscounts(Discount First, Discount Second) : Discount
     {
         public override decimal Apply(Product product) => First.Apply(product) + Second.Apply(product);
     }
---
+++ PriceCalculatorKata/Discounts/MultiplicativeDiscounts.cs
@@ -0,0 +1,14 @@
+﻿namespace PriceCalculatorKata.Discounts
+{
+    record MultiplicativeDiscounts(Discount First, Discount Second) : Discount
+    {
+        public override decimal Apply(Product product)
+        {
+            var firstDiscountAmount = First.Apply(product);
+            var discountedPrice = product.Price - firstDiscountAmount;
+            var discountedProduct = product with { Price = discountedPrice };
+            var secondDiscountAmount = Second.Apply(discountedProduct);
+            return firstDiscountAmount + secondDiscountAmount;
+        }
+    }
+}
